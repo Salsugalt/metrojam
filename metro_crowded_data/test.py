e07015726dc8e8f1288ea9af5c41fdb2f6d1bbae
Salsugalt <47024366+Salsugalt@users.noreply.github.com>
2024-09-24 08:45:03 +0900

    cm

--- metro_crowded_data/test.py
+++ metro_crowded_data/test.py
@@ -6,26 +6,56 @@ import bs4
 import re
 import xml.etree.ElementTree as ET
 from xml.etree.ElementTree import parse as ps
-page = 0
+page = 1
+perPage = 1667
 url = 'https://api.odcloud.kr/api/15071311/v1/uddi:a5158b81-27c7-4151-ba6c-b912a6f13d39'
 params = {'serviceKey' : '9bi5h25Oa1d6yB2qd+7vNhE7qWLhgJvDSr4dTv6vhlZ3rvlpNTbs6NZv/B+R/CvFLssD7Flh+jSDAYydOoaRfQ==', #decoding key
           'returnType' : 'XML',
-        'page' : page, 'perPage' : '1'}
+        'page' : page, 'perPage' : perPage}
 response = requests.get(url, params=params)
 
 data = response.text
 #pp = pprint.PrettyPrinter(indent=4)
 #print(pp.pprint(data))
 
+
+# XML 데이터를 파싱
+xml_obj = ET.fromstring(data)
+rows = []
+# 정규식
+pattern = re.compile(r"%")
+# XML 데이터를 순회하며 데이터프레임에 추가
+for row in xml_obj.findall('.//item'):
+    row_data = {}
+    for col in row.findall('.//col'):
+        if pattern.search(col.attrib['name']):
+            row_data[col.attrib['name']] = col.text
+    if row_data:  # row_data가 비어있지 않으면 추가
+        rows.append(row_data)
+
+# pddf
+df = pd.DataFrame(rows)
+print(df)
+#df.to_csv('output.csv', index=False)
+
+"""
 xml_obj = ET.fromstring(data)
 match_Cnt = xml_obj.find('matchCount').text
 rows = xml_obj.findall('.//col') #xpath 표현식
+
+
 for row in rows:
-   print(f"{row.attrib['name']} : {row.text}")
+   if row.attrib['name'] == 
+      print(f"{row.attrib['name']} : {row.text}")
    
+      
+"""
 
 
-   """
+
+
+
+"""
 xml_obj = bs4.BeautifulSoup(data, 'lxml-xml')
 matchCount = xml_obj.find('matchCount')
 page = xml_obj.find('page')
@@ -48,4 +78,5 @@ for elem in xml_obj.iter('col'):
     col_name = elem.get('name')
     col_value = elem.text
     columns[col_name] = col_value
+
 """
