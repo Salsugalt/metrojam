54e6c8d49758b28696b6bb89252ab08cf0dac36b
Salsugalt <47024366+Salsugalt@users.noreply.github.com>
2024-12-18 07:55:47 +0900

    Create PredictionApi.kt

---
+++ app/임시앱/PredictionApi.kt
@@ -0,0 +1,22 @@
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+data class PredictionRequest(
+    val direction: Boolean,
+    val day_type: Boolean,
+    val time: String,
+    val station_name: String,
+    val line: Int
+)
+
+data class PredictionResponse(
+    val median_congestion: Double?,
+    val predict_congestion: Double?,
+    val error: String?
+)
+
+interface PredictionApi {
+    @POST("/predict")
+    fun getMedianCongestion(@Body request: PredictionRequest): Call<PredictionResponse>
+}
